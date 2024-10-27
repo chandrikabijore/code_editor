91609fb48616961e635cd81b0ec04471e59a6532
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-10-27 22:07:10 +0530

    Code it like a pro

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function run(){
-
     let htmlCode = document.getElementById("html-code").value;
     let cssCode = document.getElementById("css-code").value;
     let jsCode = document.getElementById("js-code").value;
